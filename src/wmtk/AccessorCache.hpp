8070ac53aaea4db964fcc5cfec70ff51eda04f41
Michael Tao <michael.tao@nyu.edu>
2023-06-24 21:42:05 -0400

    stuff?

--- src/wmtk/AccessorCache.hpp
+++ src/wmtk/AccessorCache.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 namespace wmtk {
-    template <typename T>
-    class AccessorCache {
-    };
-}
+template <typename T>
+class AccessorCache
+{
+    std::vector<T> m_attribute;
+};
+} // namespace wmtk
