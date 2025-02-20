3bbb7fb86486e772c8b5718a3d5040b5e74301eb
Michael Tao <mtao@dgp.toronto.edu>
2025-02-20 15:17:05 -0500

    asdf

--- components/multimesh/src/wmtk/components/multimesh/MultimeshOptions.hpp
+++ components/multimesh/src/wmtk/components/multimesh/MultimeshOptions.hpp
@@ -1,50 +1,61 @@
 #pragma once
 
+#include "utils/AttributeDescription.hpp"
 #include <nlohmann/json.hpp>
+#include <wmtk/components/utils/json_macros.hpp>
 
-namespace wmtk::components {
+namespace wmtk::components::multimesh {
 
-struct MultimeshUVOptions
-{
-public:
-    std::string type;
-    std::string parent;
-    std::string child;
-    std::string name;
+
+struct MultimeshBijectionOptions {
+    std::string parent_path;
+    std::string child_path;
+WMTK_NLOHMANN_JSON_FRIEND_DECLARATION(MultimeshBijectionOptions)                                   
 };
 
-struct MultimeshBOptions
-{
-public:
+struct MultimeshBoundaryOptions {
+    std::string mesh_path;
+    int8_t boundary_dimension;
+
+    // optional attribute to store the boundary data
+    utils::AttributeDescription boundary_attribute;
+WMTK_NLOHMANN_JSON_FRIEND_DECLARATION(MultimeshBoundaryOptions)                                   
+};
+struct MultimeshAxisAlignedFusionOptions {
+
+    std::string mesh_path;
+    std::vector<bool> axes_to_fuse;
+    double epsilon;
+
+WMTK_NLOHMANN_JSON_FRIEND_DECLARATION(MultimeshAxisAlignedFusionOptions)                                   
+};
+struct MultimeshTagOptions {
+    std::string mesh_path;
+    utils::AttributeDescription tag_attribute;
+WMTK_NLOHMANN_JSON_FRIEND_DECLARATION(MultimeshTagOptions)                                   
+};
+struct MultimeshOptions {
     std::string type;
-    std::string name;
-    std::string mesh;
-    std::string tag_name;
-    nlohmann::json position;
+
+    std::variant<MultimeshBijectionOptions,MultimeshBoundaryOptions, MultimeshAxisAlignedFusionOptions, MultimeshTagOptions> options;
+WMTK_NLOHMANN_JSON_FRIEND_DECLARATION(MultimeshOptions)                                   
 };
 
-struct MultimeshTOptions
-{
-public:
-    std::string type;
-    std::string name;
-    std::string mesh;
-    nlohmann::json position;
-    std::string tag;
-    int64_t tag_value;
-    int64_t primitive;
-};
 
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(MultimeshUVOptions, type, parent, child, name);
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(MultimeshBOptions, type, name, mesh, position, tag_name);
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(
-    MultimeshTOptions,
-    type,
-    name,
-    mesh,
-    position,
-    tag,
-    tag_value,
-    primitive);
+
+
+
+
+//NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(MultimeshUVOptions, type, parent, child, name);
+//NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(MultimeshBOptions, type, name, mesh, position, tag_name);
+//NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(
+//    MultimeshTOptions,
+//    type,
+//    name,
+//    mesh,
+//    position,
+//    tag,
+//    tag_value,
+//    primitive);
 
 } // namespace wmtk::components
