a49400f6720cddd56fa356a278fd0d314fbd4684
mtao <michael.tao@cims.nyu.edu>
2025-05-05 11:14:19 +0000

    Deploying to gh-pages from @ mtao/wildmeshing-toolkit@3b5cb0847aded47001184801ef424b4ca216175b 🚀

--- classwmtk_1_1attribute_1_1_typed_attribute_handle.js
+++ classwmtk_1_1attribute_1_1_typed_attribute_handle.js
@@ -1,7 +1,6 @@
 var classwmtk_1_1attribute_1_1_typed_attribute_handle =
 [
     [ "Type", "classwmtk_1_1attribute_1_1_typed_attribute_handle.html#a0615d4ff21dd321382a239c9bccacc87", null ],
-    [ "value_type", "classwmtk_1_1attribute_1_1_typed_attribute_handle.html#a021b40fd146ac24bf8fdaa343174667d", null ],
     [ "TypedAttributeHandle", "classwmtk_1_1attribute_1_1_typed_attribute_handle.html#a16a63f4f6200ee08f8e126de3b3c05d1", null ],
     [ "TypedAttributeHandle", "classwmtk_1_1attribute_1_1_typed_attribute_handle.html#a7f2d28f71b9b74e1562c31e26c7c415e", null ],
     [ "TypedAttributeHandle", "classwmtk_1_1attribute_1_1_typed_attribute_handle.html#aa107f6003041ef961f4c4f9189214149", null ],
@@ -16,10 +15,9 @@ var classwmtk_1_1attribute_1_1_typed_attribute_handle =
     [ "operator=", "classwmtk_1_1attribute_1_1_typed_attribute_handle.html#ab937661cf06c465ecf5a8ad60f3fd7c7", null ],
     [ "operator==", "classwmtk_1_1attribute_1_1_typed_attribute_handle.html#a0d95a76412b6bdcf9f04da85fc7e348d", null ],
     [ "primitive_type", "classwmtk_1_1attribute_1_1_typed_attribute_handle.html#a07ea6bc844be27a48fde619a3578b128", null ],
-    [ "Accessor", "classwmtk_1_1attribute_1_1_typed_attribute_handle.html#a3b700009b89852955aced5e7940d0261", null ],
-    [ "AccessorBase", "classwmtk_1_1attribute_1_1_typed_attribute_handle.html#a45a470617083e2afdca1b76b5c0377c7", null ],
+    [ "Accessor", "classwmtk_1_1attribute_1_1_typed_attribute_handle.html#a4d3236ba9c2947eeca523201578de58c", null ],
     [ "AttributeManager", "classwmtk_1_1attribute_1_1_typed_attribute_handle.html#a667110497747903023648e2ef6ff02ea", null ],
-    [ "MeshAttributes< T >", "classwmtk_1_1attribute_1_1_typed_attribute_handle.html#a8665d99a647fdffe278f2e4745ffd726", null ],
+    [ "TypedAttributeManager< T >", "classwmtk_1_1attribute_1_1_typed_attribute_handle.html#a115c9abf46e7b7f972ce36a19aabd2bf", null ],
     [ "wmtk::hash< TypedAttributeHandle< T > >", "classwmtk_1_1attribute_1_1_typed_attribute_handle.html#a9bda601960cf5f161ccd0250714d1d22", null ],
     [ "wmtk::Mesh", "classwmtk_1_1attribute_1_1_typed_attribute_handle.html#a40e16ebf947f9980244ad9e954e38aa3", null ],
     [ "m_base_handle", "classwmtk_1_1attribute_1_1_typed_attribute_handle.html#ade337a19e83a2c6a2386dc8e5386bb89", null ],
--- search/related_5.js
+++ search/related_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['internal_3a_3aattributetransactionstack_3c_20t_20_3e_0',['AttributeTransactionStack&lt; T &gt;',['../classwmtk_1_1attribute_1_1_attribute.html#a67ca1882194ac7bc17970a4f18a2405b',1,'wmtk::attribute::Attribute']]],
+  ['indexflagaccessor_3c_20meshtype_20_3e_0',['IndexFlagAccessor&lt; MeshType &gt;',['../classwmtk_1_1attribute_1_1_accessor.html#ad510de705a5a6a8ac2667231f4dae824',1,'wmtk::attribute::Accessor']]],
   ['internal_3a_3acheckpointscope_1',['CheckpointScope',['../classwmtk_1_1attribute_1_1_attribute_manager.html#a0f7f55611b12d4f70b0b85afec011ab4',1,'wmtk::attribute::AttributeManager']]],
   ['internal_3a_3acollapsealternatefacetdata_2',['CollapseAlternateFacetData',['../classwmtk_1_1operations_1_1_edge_operation_data.html#af026bfad8c6f74e23e9b7d2ca61b9dd8',1,'wmtk::operations::EdgeOperationData']]],
   ['internal_3a_3asplitalternatefacetdata_3',['SplitAlternateFacetData',['../classwmtk_1_1operations_1_1_edge_operation_data.html#a81a7a1c16ff4f241e4f6474df6bc2674',1,'wmtk::operations::EdgeOperationData']]],
