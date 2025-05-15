279b390ca11aa7ff3a89aa4d724b11dbae6fc9fe
Michael Tao <mtao@dgp.toronto.edu>
2025-05-15 11:40:54 -0400

    minor

--- components/multimesh/src/wmtk/components/multimesh/from_manifold_decomposition.hpp
+++ components/multimesh/src/wmtk/components/multimesh/from_manifold_decomposition.hpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <wmtk/Tuple.hpp>
 #include <wmtk/Types.hpp>
+#include <wmtk/dart/Dart.hpp>
 
 namespace wmtk {
 class Mesh;
@@ -16,8 +17,22 @@ class MeshAttributeHandle;
 }
 } // namespace wmtk
 namespace wmtk::components::multimesh {
+    struct NonManifoldCascade {
+
+
+        // simplices with respect to the parent mesh's vertex indices
+        MatrixXl S;
+
+        std::vector<dart::Dart> parent_map;
+
+
+        NonManifoldCascade* m_parent = nullptr;
+        std::unique_ptr<NonManifoldCascade> m_child = nullptr;
+    };
 
 // returns the new root mesh (the input mesh becomes a child mesh)
+    NonManifoldCascade cascade_from_manifold_decomposition(wmtk::Mesh& m);
+
     std::vector<std::shared_ptr<Mesh>> from_manifold_decomposition(wmtk::Mesh& m);
 
 } // namespace wmtk::components::multimesh
