ff3bea36c681c4bd5c9a4e19dccf147501c50a70
Michael Tao <michael.tao@nyu.edu>
2025-05-16 09:49:57 -0400

    adding eigen compactification tool

---
+++ src/wmtk/utils/internal/compactify_eigen_indices.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <Eigen/Core>
+#include <map>
+
+namespace wmtk::utils::internal {
+
+template <typename Derived>
+auto compactify_eigen_indices(Eigen::MatrixBase<Derived>& M)
+{
+    using Scalar = typename Derived::Scalar;
+    std::vector<Scalar> unindexer;
+    unindexer.reserve(M.size());
+    std::map<Scalar, Scalar> indexer;
+    for (Eigen::Index r = 0; r < M.rows(); ++r) {
+        for (Eigen::Index c = 0; c < M.cols(); ++c) {
+            indexer.try_emplace(M(r, c), indexer.size());
+        }
+    }
+    return M.unaryExpr([&](Scalar i) -> int64_t { return indexer.at(i); }).eval();
+}
+} // namespace wmtk::utils::internal
