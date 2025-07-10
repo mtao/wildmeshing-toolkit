dba5b47d53c8b1e43da4bce2ce6e59d3a78dfacd
mtao <michael.tao@cims.nyu.edu>
2025-07-10 16:09:12 +0000

    Deploying to gh-pages from @ mtao/wildmeshing-toolkit@44d58a43e133f57e1a19ec34ffc8a6f4db94e90c 🚀

--- classwmtk_1_1_edge_mesh_1_1_edge_mesh_operation_executor.js
+++ classwmtk_1_1_edge_mesh_1_1_edge_mesh_operation_executor.js
@@ -4,14 +4,13 @@ var classwmtk_1_1_edge_mesh_1_1_edge_mesh_operation_executor =
     [ "collapse_edge", "classwmtk_1_1_edge_mesh_1_1_edge_mesh_operation_executor.html#a6bdc1a1b0f87a7d34095177947029f5c", null ],
     [ "collapse_edge_single_mesh", "classwmtk_1_1_edge_mesh_1_1_edge_mesh_operation_executor.html#a1205a3f179088ad8391dc241f6a579f4", null ],
     [ "delete_simplices", "classwmtk_1_1_edge_mesh_1_1_edge_mesh_operation_executor.html#ae757030d0b82b51c8b16760155c9f96a", null ],
-    [ "incident_vids", "classwmtk_1_1_edge_mesh_1_1_edge_mesh_operation_executor.html#a503c54f3a0f4247966c08eaf8d6a44ec", null ],
-    [ "operating_edge_id", "classwmtk_1_1_edge_mesh_1_1_edge_mesh_operation_executor.html#a5a4978ead02f9d631e9a345455e7ff65", null ],
+    [ "mesh", "classwmtk_1_1_edge_mesh_1_1_edge_mesh_operation_executor.html#acfaf44d5ea942cb80793f702546335e5", null ],
+    [ "mesh", "classwmtk_1_1_edge_mesh_1_1_edge_mesh_operation_executor.html#aacbca38a7dda351fcb4f0497550cec9d", null ],
     [ "request_simplex_indices", "classwmtk_1_1_edge_mesh_1_1_edge_mesh_operation_executor.html#a6998653a06ffa3f2a1c147adaaa378c4", null ],
     [ "split_edge", "classwmtk_1_1_edge_mesh_1_1_edge_mesh_operation_executor.html#af15baefa5f355f410a386fcb93001141", null ],
     [ "split_edge_single_mesh", "classwmtk_1_1_edge_mesh_1_1_edge_mesh_operation_executor.html#acb42aca716850f7017290140eef6d94e", null ],
     [ "ee_accessor", "classwmtk_1_1_edge_mesh_1_1_edge_mesh_operation_executor.html#afc67be72547a5a2bbebbc54bf7248992", null ],
     [ "ev_accessor", "classwmtk_1_1_edge_mesh_1_1_edge_mesh_operation_executor.html#a34297e152caebc9436bde1c1154163e6", null ],
     [ "flag_accessors", "classwmtk_1_1_edge_mesh_1_1_edge_mesh_operation_executor.html#a3b198c667cfd322dfa4d65894b9fe150", null ],
-    [ "m_mesh", "classwmtk_1_1_edge_mesh_1_1_edge_mesh_operation_executor.html#a0021b296dcd26aaea0e07305a7236a5e", null ],
     [ "ve_accessor", "classwmtk_1_1_edge_mesh_1_1_edge_mesh_operation_executor.html#ae0603c7c3f40b918b1f45db1defb9a4f", null ]
 ];
